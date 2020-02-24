76c80e979c5ffd3d6165ac027d970bcd903beedd
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-24 23:20:12 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,17 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
+    "Stephen Spielberg"=>1357566430,
+    "Russo Brothers"=>2281002470,
+    "James Cameron"=>2571020373,
+    "Spike Lee"=>256624217,
+    "Wachowski Siblings"=>806180282,
+    "Robert Zemeckis"=>1273838385,
+    "Quentin Tarantino"=>662738268,
+    "Martin Scorsese"=>636812242,
+    "Francis Ford Coppola"=>509719258
   }
+  
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
@@ -18,5 +28,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  pp result
 end
